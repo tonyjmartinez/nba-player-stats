094cdbe69a7cb484074260d4063a240ba869001d
Tony Martinez <ajosephmartinez@gmail.com>
2018-06-30 18:17:54 -0700

    .

--- src/containers/Nav/Search/Search.js
+++ src/containers/Nav/Search/Search.js
@@ -43,7 +43,9 @@ class Search extends Component {
             position: 'fixed',
             overflow: 'auto',
             maxHeight: '50%',
-            marginLeft: '0px'
+            marginLeft: '0px',
+            borderRadius: '5px'
+
         }
 
         const searchStyle = {
@@ -51,12 +53,14 @@ class Search extends Component {
 
         const itemStyle = {
             position: 'relative',
-            left: '20px'
+            left: '20px',
+
         }
 
         return (
                 <div  className={classes.Search}>
                 <ReactAutocomplete
+                ref={(ip)=>this.search = ip}
                 wrapperStyle={wrapperStyle}
                 inputProps={{style: style}}
 
@@ -72,16 +76,20 @@ class Search extends Component {
                     <div
                         key={item.id}
                     style={highlighted ? dropdownStyleHL: dropdownStyle}
-
+                    className={classes.Dropdown}
                     >
-                    &nbsp;&nbsp;{item.label}
+                    <span className={classes.DropItem}>{item.label}</span>
                     </div>
                 }
                 value={this.state.value}
                 onChange={e => this.setState({ value: e.target.value  })}
                 onSelect={value => this.setState({ value  })}
                 />
-                <SearchIcon style={searchStyle} size={40} color='white'/>
+                <SearchIcon style={searchStyle}
+                            size={40}
+                            color='white'
+                            onClick={() => {this.search.focus()}}
+                            />
                     </div>
 
                     )
